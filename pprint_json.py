52f595a54916a942d3aebbfe001181d1bd7bdf83
john <nergilz@yandex.ru>
2018-02-20 15:48:02 +0200

    if__name__=='__main__'  simplified

--- pprint_json.py
+++ pprint_json.py
@@ -6,16 +6,19 @@ from json.decoder import JSONDecodeError
 
 def load_data(path_to_file):
     with open(os.path.join(path_to_file), 'r') as file_handler:
-        json_content = json.load(file_handler)
-        return json_content
+        try:
+            json_content = json.load(file_handler)
+            return json_content
+        except JSONDecodeError:
+            print(' Script_Error: This is not json-file or file is empty.')
 
 
 def pretty_print_json(json_content):
-        json_content_for_print = json.dumps(json_content,
-                                            ensure_ascii=False,
-                                            sort_keys=True,
-                                            indent=2)
-        return json_content_for_print
+    json_content_for_print = json.dumps(json_content,
+                                        ensure_ascii=False,
+                                        sort_keys=True,
+                                        indent=2)
+    return json_content_for_print
 
 
 if __name__ == '__main__':
@@ -28,5 +31,3 @@ if __name__ == '__main__':
         print(' Script_Error: No filename for reading.')
     except FileNotFoundError:
         print(' Script_Error: File or path "{0}" not found.'.format(path_to_file))
-    except JSONDecodeError:
-        print(' Script_Error: This is not json-file or file is empty.')
